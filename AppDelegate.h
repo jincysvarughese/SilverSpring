56c5071dd2f0e80f5c5ca5fd58536bf123649ec0
Gison <gisongeorge@gmail.com>
2017-05-29 14:59:12 +0530

    Add files via upload
    sample commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sampleSideMenuViewController
+//
+//  Created by Gison on 16/05/17.
+//  Copyright © 2017 JSV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
